61853aa29281b48e1bd3766d8923f95d41702ca4
Callum Ahmed <1903181@uad.ac.uk>
2024-09-23 14:39:29 +0100

    Added toggle between rasteriser and raytracer mode
    Spacebar can be used to toggle between modes.
    Escape can be used to close window.
    Raytracer is currently just clearing render target as I want to add BVH before rendering geometry.

--- HelloTriangle/D3D12HelloTriangle.h
+++ HelloTriangle/D3D12HelloTriangle.h
@@ -34,6 +34,7 @@ public:
 
 private:
 	static const UINT FrameCount = 2;
+	bool m_raster = true;
 
 	struct Vertex
 	{
@@ -45,14 +46,14 @@ private:
 	CD3DX12_VIEWPORT m_viewport;
 	CD3DX12_RECT m_scissorRect;
 	ComPtr<IDXGISwapChain3> m_swapChain;
-	ComPtr<ID3D12Device> m_device;
+	ComPtr<ID3D12Device5> m_device;
 	ComPtr<ID3D12Resource> m_renderTargets[FrameCount];
 	ComPtr<ID3D12CommandAllocator> m_commandAllocator;
 	ComPtr<ID3D12CommandQueue> m_commandQueue;
 	ComPtr<ID3D12RootSignature> m_rootSignature;
 	ComPtr<ID3D12DescriptorHeap> m_rtvHeap;
 	ComPtr<ID3D12PipelineState> m_pipelineState;
-	ComPtr<ID3D12GraphicsCommandList> m_commandList;
+	ComPtr<ID3D12GraphicsCommandList4> m_commandList;
 	UINT m_rtvDescriptorSize;
 
 	// App resources.
@@ -65,6 +66,8 @@ private:
 	ComPtr<ID3D12Fence> m_fence;
 	UINT64 m_fenceValue;
 
+	virtual void OnKeyUp(UINT8 key);
+	void CheckRaytracingSupport();
 	void LoadPipeline();
 	void LoadAssets();
 	void PopulateCommandList();
